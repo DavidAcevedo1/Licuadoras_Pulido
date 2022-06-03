ce565341d1c6ad58b23c19e77fc39b531011d7b8
David Acevedo <100775468+DavidAcevedo1@users.noreply.github.com>
2022-06-03 15:23:30 -0500

    recuperar contraseña

--- gestion/urls.py
+++ gestion/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
-from gestion.views import inicio, login
+from gestion.views import inicio, login, password_reset, contraseña_email, contraseña_correo, contraseña_confirmar
 from administrador.views import inicioadmin
 from django.conf import settings
 from django.conf.urls.static import static
@@ -29,5 +29,15 @@ urlpatterns = [
     # Logueo
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='usuario-login'),
     path('logout/', auth_views.LogoutView.as_view(), name='usuario-logout'),
+    # Recuperación
+    path('reset/password_reset', password_reset, {'template_name':'recuperacion/contraseña-correo.html',
+        'email_template':'recuperacion/contraseña-email.html'},
+        name='password_reset'),
+    path('reset/contraseña-correo', contraseña_correo, {'template_name':'recuperacion/contraseña-correo.html',},
+        name='contraseña-correo'),
+    path('reset/(?P<uidb64>[0-9A-z_\-]+)/(?P<token>.+)/$', contraseña_confirmar, {'template_name':'recuperacion/contraseña-confirmar.html',},
+        name='contraseña-confirmar'),
+    # path('reset/done', contraseña_confirmar, {'template_name':'recuperacion/contraseña-correo.html',},
+    #     name='contraseña-correo'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
