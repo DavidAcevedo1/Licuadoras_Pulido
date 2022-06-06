4886ff75fc7f183aac8049e76642f290d698e2f0
Franklin <facceres1@misena.edu.co>
2022-06-06 11:40:21 -0500

    centrar la margen de los botones de administrdor

--- gestion/settings.py
+++ gestion/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k5%1h-fsycdzyps13o&$d3k2+t84$upa@e#0)fc$*b6wpyba6-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
