890aece504479fcc4cc749feb579367d8604e29a
JorgeBl23 <JorgeBl23@users.noreply.github.com>
2022-09-11 22:11:00 -0500

    contraste, head
    contraste, head

--- gestion/static/js/tabla-proyecto.js
+++ gestion/static/js/tabla-proyecto.js
@@ -11,13 +11,13 @@ $(document).ready(function () {
         buttons:[ 
 			{
 				extend:    'excelHtml5',
-				text:      '<i class="fas fa-file-excel"></i> ',
+				text:      '<i class="fas fa-file-excel"> Exel</i> ',
 				titleAttr: 'Exportar a Excel',
 				className: 'btn btn-success'
 			},
 			{
 				extend:    'pdfHtml5',
-				text:      '<i class="fas fa-file-pdf"></i> ',
+				text:      '<i class="fas fa-file-pdf"> PDF</i> ',
 				titleAttr: 'Exportar a PDF',
 				className: 'btn btn-danger',
                 orientation: 'portrait',
@@ -98,7 +98,7 @@ $(document).ready(function () {
             },
 			{
 				extend:    'print',
-				text:      '<i class="fa fa-print">Imprimir</i> ',
+				text:      '<i class="fa fa-print"> Imprimir</i> ',
 				titleAttr: 'Imprimir',
 				className: 'btn btn-info'
                 
